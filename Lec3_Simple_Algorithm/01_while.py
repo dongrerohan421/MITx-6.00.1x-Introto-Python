9e165878a739f6936f6cad474c49c312d59016fa
Rohan Dongre <dongrerohan421@gmail.com>
2016-01-30 23:50:46 -0600

    L3 Problem 1

---
+++ Lec3_Simple_Algorithm/01_while.py
@@ -0,0 +1,13 @@
+'''
+MITx-6.00.1x-Introto-Python/Lec3_Simple_Algorithm Examples.
+'''
+
+print '01 Ex:'
+
+num = 0	# Initialize num variable
+while num <= 5:	# Condition
+	print num	
+	num += 1	# Increament num by +1: num++
+
+print "Outside of loop"
+print "num:",num
